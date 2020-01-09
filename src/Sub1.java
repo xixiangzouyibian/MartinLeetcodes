4fa21e91c6c34d00c2179c975a25b06ec321f188
Martin <zmbeatit@163.com>
2020-01-09 22:26:20 +0800

    init

---
+++ src/Sub1.java
@@ -0,0 +1,10 @@
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+public class Sub1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
