38dbfc310b9a1aa46476c617fcf7746f04ea5abf
Martin <zmbeatit@163.com>
2020-10-11 01:00:10 +0800

    Update for Num 121

--- src/com/martin/leetcode/editor/cn/BestTimeToBuyAndSellStock.java
+++ src/com/martin/leetcode/editor/cn/BestTimeToBuyAndSellStock.java
@@ -52,7 +52,7 @@ class Solution {
         return max;
     }*/
 
-    public int maxProfit(int[] prices) {
+/*    public int maxProfit(int[] prices) {
         if (prices.length <=1) return 0;
         int least_hold_price = prices[0], max_profile = 0;
 
@@ -61,6 +61,18 @@ class Solution {
             least_hold_price = Math.min(least_hold_price, prices[i]);
         }
         return max_profile;
+    }*/
+
+    public int maxProfit(int[] prices) {
+        if (prices.length <=1) return 0;
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
+        }
+        return dp[prices.length-1][0];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
