179caac7c24c5c14b5c7bfb3f40151683feb518d
Martin <zmbeatit@163.com>
2020-10-11 21:07:19 +0800

    Update for Num 121

--- src/com/martin/leetcode/editor/cn/BestTimeToBuyAndSellStock.java
+++ src/com/martin/leetcode/editor/cn/BestTimeToBuyAndSellStock.java
@@ -70,7 +70,7 @@ class Solution {
         dp[0][1] = -prices[0];
         for (int i = 1; i < prices.length; i++) {
             dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i-1][1], -prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], 0-prices[i]);
         }
         return dp[prices.length-1][0];
     }
