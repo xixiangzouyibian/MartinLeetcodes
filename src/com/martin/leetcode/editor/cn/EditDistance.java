8cb28a0fffabce999b7778c164c4afc2f5383bd1
Martin <zmbeatit@163.com>
2020-10-14 22:27:36 +0800

    Num 72: EditDistance.minDistance

---
+++ src/com/martin/leetcode/editor/cn/EditDistance.java
@@ -0,0 +1,75 @@
+//给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。 
+//
+// 你可以对一个单词进行如下三种操作： 
+//
+// 
+// 插入一个字符 
+// 删除一个字符 
+// 替换一个字符 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：word1 = "horse", word2 = "ros"
+//输出：3
+//解释：
+//horse -> rorse (将 'h' 替换为 'r')
+//rorse -> rose (删除 'r')
+//rose -> ros (删除 'e')
+// 
+//
+// 示例 2： 
+//
+// 输入：word1 = "intention", word2 = "execution"
+//输出：5
+//解释：
+//intention -> inention (删除 't')
+//inention -> enention (将 'i' 替换为 'e')
+//enention -> exention (将 'n' 替换为 'x')
+//exention -> exection (将 'n' 替换为 'c')
+//exection -> execution (插入 'u')
+// 
+// Related Topics 字符串 动态规划 
+// 👍 1193 👎 0
+
+  
+package com.martin.leetcode.editor.cn;
+  
+  
+public class EditDistance{
+
+    public static void main(String[] args) {
+        Solution solution = new EditDistance().new Solution();
+    }
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
