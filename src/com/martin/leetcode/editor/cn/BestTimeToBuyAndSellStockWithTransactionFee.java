1c4b522b66d6fe845b39971052e47bad1aafd266
Martin <zmbeatit@163.com>
2020-12-17 22:16:09 +0800

    Update Num 714: BestTimeToBuyAndSellStockWithTransactionFee.maxProfit

--- src/com/martin/leetcode/editor/cn/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/com/martin/leetcode/editor/cn/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -29,8 +29,8 @@
 
   
 package com.martin.leetcode.editor.cn;
-  
-  
+
+
 public class BestTimeToBuyAndSellStockWithTransactionFee{
 
     public static void main(String[] args) {
@@ -40,16 +40,32 @@ public class BestTimeToBuyAndSellStockWithTransactionFee{
 
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-    public int maxProfit(int[] prices, int fee) {
-        if (prices.length <=1) return 0;
-        int[][] dp = new int[prices.length][2];
+        // 0 is not hold, 1 is hold
+/*    public int maxProfit(int[] prices, int fee) {
+        int len = prices.length;
+        if (len <= 1) return 0;
+
+        int[][] dp = new int[len][2];
         dp[0][0] = 0;
         dp[0][1] = -prices[0];
         for (int i = 1; i < prices.length; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i] - fee);
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]+prices[i]-fee);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0]-prices[i]);
         }
-        return dp[prices.length-1][0];
+        return dp[len-1][0];
+    }*/
+
+    public int maxProfit(int[] prices, int fee) {
+        int len = prices.length;
+        if (len <= 1) return 0;
+
+        int a = 0;
+        int b = -prices[0];
+        for (int i = 1; i < len; i++) {
+            a = Math.max(a, b+prices[i]-fee);
+            b = Math.max(b, a-prices[i]);
+        }
+        return a;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
