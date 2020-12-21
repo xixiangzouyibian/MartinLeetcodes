831ad49905fe637313a83891d41b6103fec2f1f7
Martin <zmbeatit@163.com>
2020-12-21 23:39:04 +0800

    Update Num 206: ReverseLinkedList.reverseList

--- src/com/martin/leetcode/editor/cn/ReverseLinkedList.java
+++ src/com/martin/leetcode/editor/cn/ReverseLinkedList.java
@@ -21,13 +21,13 @@ public class ReverseLinkedList{
     
     //leetcode submit region begin(Prohibit modification and deletion)
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+ * Definition for singly-linked list.**/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
 class Solution {
 /*    public ListNode reverseList(ListNode head) {
         ListNode pre = null;
