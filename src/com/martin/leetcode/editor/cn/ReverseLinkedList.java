424be2735b02e67a8a41d2aa9c30265b4983a2df
Martin <zmbeatit@163.com>
2020-12-21 23:38:32 +0800

    Num 206: ReverseLinkedList.reverseList

---
+++ src/com/martin/leetcode/editor/cn/ReverseLinkedList.java
@@ -0,0 +1,55 @@
+//反转一个单链表。 
+//
+// 示例: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL 
+//
+// 进阶: 
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？ 
+// Related Topics 链表 
+// 👍 1408 👎 0
+
+
+package com.martin.leetcode.editor.cn;
+
+
+public class ReverseLinkedList{
+    public static void main(String[] args) {
+       Solution solution = new ReverseLinkedList().new Solution();
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+/*    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }*/
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode cur = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return cur;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
