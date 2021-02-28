901630058ceedebd96523ade84dafd833e0bfa1f
Martin <zmbeatit@163.com>
2021-02-28 18:13:39 +0800

    Num 109: ConvertSortedListToBinarySearchTree.sortedListToBST

---
+++ src/com/martin/leetcode/editor/cn/ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,131 @@
+//给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。 
+//
+// 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。 
+//
+// 示例: 
+//
+// 给定的有序链表： [-10, -3, 0, 5, 9],
+//
+//一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+//
+//      0
+//     / \
+//   -3   9
+//   /   /
+// -10  5
+// 
+// Related Topics 深度优先搜索 链表 
+// 👍 473 👎 0
+
+
+package com.martin.leetcode.editor.cn;
+
+
+public class ConvertSortedListToBinarySearchTree{
+    public static void main(String[] args) {
+       Solution solution = new ConvertSortedListToBinarySearchTree().new Solution();
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+/*    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        } else if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+
+        ListNode slow = head;
+        ListNode quick = head;
+        ListNode pre = null;
+        while (quick != null && quick.next != null) {
+            pre = slow;
+            slow = slow.next;
+            quick = quick.next.next;
+        }
+        pre.next = null;
+
+        TreeNode root = new TreeNode(slow.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }*/
+
+    ListNode head;
+    public TreeNode sortedListToBST(ListNode head) {
+        this.head = head;
+        int len = getLen(head);
+        return sort(0, len-1);
+    }
+
+    public int getLen(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+
+    public TreeNode sort(int l, int r) {
+        if (l > r) return null;
+
+        TreeNode root = new TreeNode();
+        int mid = l + r + 1 >> 1;
+        root.left = sort(l, mid-1);
+        root.val = head.val;
+        head = head.next;
+        root.right = sort(mid+1, r);
+        return root;
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
