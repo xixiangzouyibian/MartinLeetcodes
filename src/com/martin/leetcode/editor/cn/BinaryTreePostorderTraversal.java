150beb113d398144372d47b996492ae0cc9496b7
Martin <zmbeatit@163.com>
2021-04-05 21:58:56 +0800

    Num 145: BinaryTreePostorderTraversal.postorderTraversal

---
+++ src/com/martin/leetcode/editor/cn/BinaryTreePostorderTraversal.java
@@ -0,0 +1,80 @@
+//给定一个二叉树，返回它的 后序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]  
+//   1
+//    \
+//     2
+//    /
+//   3 
+//
+//输出: [3,2,1] 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 
+// 👍 561 👎 0
+
+
+package com.martin.leetcode.editor.cn;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreePostorderTraversal{
+    public static void main(String[] args) {
+       Solution solution = new BinaryTreePostorderTraversal().new Solution();
+    }
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                res.add(0, root.val);
+                root = root.right;
+            } else {
+                TreeNode node = stack.pop();
+                root = node.left;
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
