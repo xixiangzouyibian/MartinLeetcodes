7ecd03811d7fd2df4732c53d111c3119f18c5274
Martin <zmbeatit@163.com>
2021-04-10 23:01:06 +0800

    Num 263: UglyNumber.isUgly

---
+++ src/com/martin/leetcode/editor/cn/UglyNumber.java
@@ -0,0 +1,82 @@
+//给你一个整数 n ，请你判断 n 是否为 丑数 。如果是，返回 true ；否则，返回 false 。 
+//
+// 丑数 就是只包含质因数 2、3 和/或 5 的正整数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 6
+//输出：true
+//解释：6 = 2 × 3 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 8
+//输出：true
+//解释：8 = 2 × 2 × 2
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：n = 14
+//输出：false
+//解释：14 不是丑数，因为它包含了另外一个质因数 7 。
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：n = 1
+//输出：true
+//解释：1 通常被视为丑数。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -231 <= n <= 231 - 1 
+// 
+// Related Topics 数学 
+// 👍 242 👎 0
+
+
+package com.martin.leetcode.editor.cn;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UglyNumber{
+    public static void main(String[] args) {
+       Solution solution = new UglyNumber().new Solution();
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isUgly(int n) {
+        List<Integer> list = Arrays.asList(2, 3, 5);
+        if (n <= 0) return false;
+
+        while (n != 1) {
+            boolean flag = false;
+            for (int c : list) {
+                if (n % c == 0) {
+                    n = n / c;
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) return false;
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
