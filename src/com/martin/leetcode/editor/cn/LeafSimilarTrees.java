586e64c00f8ad171c0d345cdcbdb2299870c0c51
Martin <zmbeatit@163.com>
2021-05-10 20:53:32 +0800

    Num 872: LeafSimilarTrees.leafSimilar

---
+++ src/com/martin/leetcode/editor/cn/LeafSimilarTrees.java
@@ -0,0 +1,148 @@
+//请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。 
+//
+// 
+//
+// 举个例子，如上图所示，给定一棵叶值序列为 (6, 7, 4, 9, 8) 的树。 
+//
+// 如果有两棵二叉树的叶值序列是相同，那么我们就认为它们是 叶相似 的。 
+//
+// 如果给定的两个根结点分别为 root1 和 root2 的树是叶相似的，则返回 true；否则返回 false 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,nul
+//l,null,null,null,9,8]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root1 = [1], root2 = [1]
+//输出：true
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root1 = [1], root2 = [2]
+//输出：false
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：root1 = [1,2], root2 = [2,2]
+//输出：true
+// 
+//
+// 示例 5： 
+//
+// 
+//
+// 
+//输入：root1 = [1,2,3], root2 = [1,3,2]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 给定的两棵树可能会有 1 到 200 个结点。 
+// 给定的两棵树上的值介于 0 到 200 之间。 
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 139 👎 0
+
+
+package com.martin.leetcode.editor.cn;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class LeafSimilarTrees{
+    public static void main(String[] args) {
+       Solution solution = new LeafSimilarTrees().new Solution();
+    }
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        iterate(root1, l1);
+        iterate(root2, l2);
+
+        if (l1.size() != l2.size()) return false;
+        for (int i = 0; i < l1.size(); i++) {
+            if (l1.get(i) != l2.get(i)) return false;
+        }
+        return true;
+    }
+
+/*    private void dfs(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        if (isLeaf(node)) {
+            list.add(node.val);
+        }
+        dfs(node.left, list);
+        dfs(node.right, list);
+    }*/
+
+    private void iterate(TreeNode node, List<Integer> list) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            TreeNode cur = stack.pop();
+            if (cur.left == null && cur.right == null) list.add(cur.val);
+            node = cur.right;
+        }
+    }
+
+    private boolean isLeaf(TreeNode node) {
+        return node != null && node.left == null && node.right == null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
